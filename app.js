7132b1dfaabc7e8c33f7aead471866beb3e8df26
Kris Hale <karissa.hale72@gmail.com>
2018-02-12 18:57:58 -0500

    changed font to fontFamily

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 
 myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value;
+  myHeading.style.fontFamily = myTextInput.value;
 });
